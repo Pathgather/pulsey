931b35f3f1a66456b20b10326fb26d4a3873ba0d
ERIC DUFFY <dufstar@ERICs-MacBook-Air.local>
2016-01-06 22:43:38 -0500

    initial commit

---
+++ pulsey.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Pulsey extends React.Component {
+  render() {
+    return (
+      <div>
+        yebo!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Pulsey />, document.getElementById('pulsey'));
