9720c56c75c4beac7e1ce35942cde283037e1a47
ERIC DUFFY <dufstar@ERICs-MacBook-Air.local>
2016-01-08 20:22:51 -0500

    remove node_modules directory from git tracking

--- dist/pulsey.js
+++ dist/pulsey.js
@@ -42,7 +42,55 @@ var Pulsey = function (_React$Component) {
   return Pulsey;
 }(_react2.default.Component);
 
-_reactDom2.default.render(_react2.default.createElement(Pulsey, null), document.getElementById('pulsey'));
+// Initialize array for pulsey objects
+
+var pulseyObjects = [];
+
+// Create array of user-selected dom anchors
+var pulseyAnchors = document.getElementsByClassName('pulsey-anchor');
+
+// Initialize pulsey objects with data based on html anchors
+function initPulseyObjects() {
+  for (var i = 0; i < pulseyAnchors.length; i++) {
+    var styles = window.getComputedStyle(pulseyAnchors[i], null),
+        header = pulseyAnchors[i].getAttribute('data-ps-header'),
+        note = pulseyAnchors[i].getAttribute('data-ps-note'),
+        step = parseInt(pulseyAnchors[i].getAttribute('data-ps-step')),
+        custom = pulseyAnchors[i].getAttribute('data-ps-custom');
+    pulseyObjects[i] = {
+      dot: {
+        id: i,
+        clicked: false,
+        coordinates: pulseyAnchors[i].getBoundingClientRect(),
+        fixed: anchorStyles.getPropertyValue('position') === "fixed"
+      },
+      tooltip: {
+        header: header ? header : options.tooltip.content.header,
+        note: note ? note : options.tooltip.content.note
+      }
+    };
+  }
+}
+
+var options = {};
+
+var styles = {};
+
+function pulsey() {
+  initPulseyObjects();
+  localStorage.setItem("nextStep", options.firstStep);
+  _reactDom2.default.render(_react2.default.createElement(Pulsey, { p: pulseyObjects }), document.getElementById('pulsy-tour'));
+}
+
+window.onresize = function renderResize() {
+  pulsy();
+};
+window.onscroll = function renderScroll() {
+  pulsy();
+};
+
+// Launch pulsey
+pulsy();
 },{"react":159,"react-dom":3}],2:[function(require,module,exports){
 // shim for using process in browser
 
--- pulsey.js
+++ pulsey.js
@@ -11,4 +11,55 @@ class Pulsey extends React.Component {
   }
 }
 
-ReactDOM.render(<Pulsey />, document.getElementById('pulsey'));
+// Initialize array for pulsey objects
+var pulseyObjects = []
+
+// Create array of user-selected dom anchors
+var pulseyAnchors = document.getElementsByClassName('pulsey-anchor');
+
+// Initialize pulsey objects with data based on html anchors
+function initPulseyObjects() {
+  for (var i=0;i<pulseyAnchors.length;i++) {
+    var styles = window.getComputedStyle(pulseyAnchors[i],null),
+        header = pulseyAnchors[i].getAttribute('data-ps-header'),
+        note = pulseyAnchors[i].getAttribute('data-ps-note'),
+        step = parseInt(pulseyAnchors[i].getAttribute('data-ps-step')),
+        custom = pulseyAnchors[i].getAttribute('data-ps-custom');
+    pulseyObjects[i] = {
+      dot: {
+        id: i,
+        clicked: false,
+        coordinates: pulseyAnchors[i].getBoundingClientRect(),
+        fixed: anchorStyles.getPropertyValue('position') === "fixed",
+      },
+      tooltip: {
+        header: header ? header : options.tooltip.content.header,
+        note: note ? note : options.tooltip.content.note,
+      }
+    }
+  }
+}
+
+var options = {
+
+}
+
+var styles = {
+
+}
+
+function pulsey() {
+  initPulseyObjects();
+  localStorage.setItem("nextStep", options.firstStep);
+  ReactDOM.render(<Pulsey p={pulseyObjects} />, document.getElementById('pulsy-tour'));
+}
+
+window.onresize = function renderResize() {
+  pulsy();
+}
+window.onscroll = function renderScroll() {
+  pulsy();
+}
+
+// Launch pulsey
+pulsy();
